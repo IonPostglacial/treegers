6f7dee5f6b3e2136dc45ea230a027c0401994895
Nicolas Galipot <nicolas.galipot@gmail.com>
2016-10-01 13:49:19 +0200

    Make entities controllable with only left click.

--- Source/game/systems/ControledSystem.hx
+++ Source/game/systems/ControledSystem.hx
@@ -28,22 +28,34 @@ class ControledNode extends Node<ControledNode> {
 class ControledSystem extends ListIteratingSystem<ControledNode> {
 	var stage:Stage;
 	var events:Array<Order>;
+	var pointedPosition:Position;
 
 	public function new(stage:Stage) {
 		this.stage = stage;
 		this.events = [];
 		Lib.current.addEventListener(MouseEvent.CLICK, function(e) {
 			var mousePosition = Shape.pointToPosition(new openfl.geom.Point(e.stageX, e.stageY), stage.grid.radius);
-			this.events.push(TargetSelected(mousePosition));
-		});
-		Lib.current.addEventListener(MouseEvent.RIGHT_CLICK, function(e) {
-			var mousePosition = Shape.pointToPosition(new openfl.geom.Point(e.stageX, e.stageY), stage.grid.radius);
-			this.events.push(MovementOrdered(mousePosition));
+			pointedPosition = mousePosition;
 		});
 		super(ControledNode, updateNode);
 	}
 
 	override function update(deltaTime:Float) {
+		if (pointedPosition != null) {
+			var targetSelected = false;
+			for (node in nodeList) {
+				if (node.position.equals(pointedPosition)) {
+					targetSelected = true;
+					break;
+				}
+			}
+			if (targetSelected) {
+				events.push(TargetSelected(pointedPosition.copy()));
+			} else {
+				events.push(MovementOrdered(pointedPosition.copy()));
+			}
+			pointedPosition = null;
+		}
 		super.update(deltaTime);
 		events = [];
 	}
@@ -56,7 +68,7 @@ class ControledSystem extends ListIteratingSystem<ControledNode> {
 					node.controled.actions = [new Move(stage, node.entity, goal)];
 				}
 			case TargetSelected(position):
-				node.controled.selected = node.position.equals(position);
+				node.controled.selected = !node.controled.selected && node.position.equals(position);
 			case GroupSelected(area): // TODO: implement it :p
 			case PowerOrdered(goal): // TODO: implement it :p
 			}
