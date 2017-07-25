f8d1a19a3fd7e4ec00f4ef5f0de2bf8d786bb6cb
Nicolas Galipot <nicolas.galipot@gmail.com>
2017-07-25 15:11:50 +0200

    Fix coding style inconsistency.

--- Source/game/actions/IAction.hx
+++ Source/game/actions/IAction.hx
@@ -5,6 +5,7 @@ import ash.core.Entity;
 import game.nodes.ActionedNode;
 import game.map.WorldMap;
 
+
 interface IAction {
 	var done(get, never):Bool;
 	function execute(worldMap:WorldMap, node:ActionedNode, deltaTime:Float):Void;
